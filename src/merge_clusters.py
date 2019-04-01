8f09cac004279fcab2bc84cfa14a17a949a172be
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-04-01 16:45:08 -0300

    Pre final merge function
    This versions depends on global architectures to be completed.

--- src/merge_clusters.py
+++ src/merge_clusters.py
@@ -1,4 +1,4 @@
-from utils import dist
+from utils import dist, weighted_mean
 
 merge_threshold = 0 # Corrigir
 
@@ -6,8 +6,11 @@ def merge_clusters(win_micro_cluster, overlaping_micro_clusters):
     for micro_cluster in overlaping_micro_clusters:
         if dist(micro_cluster, win_micro_cluster) < merge_threshold:
             merge(micro_cluster, win_micro_cluster)
+    pass
 
 
 def merge(cluster_a, cluster_b):
-
+    new_cluster_centroid = [weighted_mean(a, b, cluster_a['n'], cluster_b['n']) for a, b in zip(cluster_a['C'], cluster_b['C'])]
+    new_cluster_radius = dist(cluster_a['C'], cluster_b['C']) + max(cluster_a['r'], cluster_b['r'])
+    new_cluster = {'n': cluster_a['C'] + cluster_b['C'], 'r': new_cluster_radius, 'C':new_cluster_centroid}
     pass
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,7 @@ def dist(v1, v2):
     dist = [(a - b)**2 for a, b in zip(v1, v2)]
     dist = sqrt(sum(dist))
     return dist
+
+
+def weighted_mean(a, b, mean_a, mean_b):
+    return ((mean_a * a + mean_b * b)/(mean_a + mean_b))
