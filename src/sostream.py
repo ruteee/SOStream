2440212f54e2c63b957db430c04f5310764a762d
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-04-01 22:24:04 -0300

    Base structure for SOStream

---
+++ src/sostream.py
@@ -0,0 +1,33 @@
+from utils import min_dist, dist
+from find_neighbors import find_neighbors
+from update_cluster import updateCluster
+from find_overlap import find_overlap
+from merge_clusters import merge_clusters
+# from new_cluster import new_cluster
+
+# Mock new cluster function
+def new_cluster(vt):
+    pass
+
+class SOStream:
+
+    def __init__(self, alpha = 0.1, min_pts = 10):
+        self.alpha = alpha
+        self.min_pts = min_pts
+        self.M = list()
+
+    def process(self, vt):
+        winner_micro_cluster = min_dist(vt, self.M[-1])
+        if len(self.M[-1]) >= self.min_pts:
+            winner_neighborhood = find_neighbors(winner_micro_cluster, self.min_pts, self.M[-1])
+            if dist(vt, winner_micro_cluster['C'] < winner_micro_cluster['r']):
+                updateCluster(winner_micro_cluster, vt, self.alpha, winner_neighborhood)
+            else:
+                new_cluster(vt)
+            overlap = find_overlap(winner_micro_cluster, winner_neighborhood)
+            if len(overlap) > 0:
+                merge_clusters(winner_micro_cluster, overlap)
+        else:
+            new_cluster(vt)
+
+    pass
