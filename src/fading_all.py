aa0a4d87a23aafa02ed04b9c808213868ac31105
AndressaStefany <astefanysoliveira@gmail.com>
2019-04-02 18:40:58 -0300

    function fading all

---
+++ src/fading_all.py
@@ -0,0 +1,7 @@
+def fadingAll(M, t_current, var_lambda, fadeThreshold):
+  M_copy = M.copy()
+  for microcluster in M_copy:
+    t = t_current - microcluster['t']
+    fading = microcluster['n']*2**(-var_lambda*t)
+    if fading < fadeThreshold:
+      M.remove(microcluster)
