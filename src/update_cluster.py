35724aa6c2c3e6333bbecbc7a1c187400545d0c4
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-04-03 11:42:40 -0300

    Final update version

--- src/update_cluster.py
+++ src/update_cluster.py
@@ -3,9 +3,9 @@ from utils import dist
 
 
 def updateCluster(win_micro_cluster, vt, alpha, winner_neighborhood):
-    win_micro_cluster['C'] = [] # new centroid
+    win_micro_cluster['C'] = [(win_micro_cluster['n'] * (wi + vi)) / (win_micro_cluster['n'] + 1) for wi, vi in zip(win_micro_cluster['C'], vt)]
     win_micro_cluster['n'] += 1
     width_neighbor = win_micro_cluster['r'] ** 2
     for neighbor_micro_cluster in winner_neighborhood:
-        influence = exp(-(dist(neighbor_micro_cluster, win_micro_cluster)/(2 * width_neighbor)))
+        influence = exp(-(dist(neighbor_micro_cluster['C'], win_micro_cluster['C'])/(2 * width_neighbor)))
         neighbor_micro_cluster['C'] = [a + alpha*influence*(b-a) for a, b in zip(neighbor_micro_cluster['C'], win_micro_cluster['C'])]
