e85837f98e22df99514e63b7fdb2790af2cd60fb
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-04-03 11:44:11 -0300

    Joining all functions

--- src/new_cluster.py
+++ src/new_cluster.py
@@ -1,5 +1,4 @@
-def newCluster(M, vt):
-    new_cluster = {'n': 1, 'r': 0, 'C':vt}
-    new_mt = M[-1].copy()
-    new_mt.append(new_cluster)
-    M.append(new_mt)
+
+def newCluster(vt):
+    return {'n': 1, 'r': 0, 'C': vt}
+
--- src/sostream.py
+++ src/sostream.py
@@ -8,23 +8,28 @@ from new_cluster import newCluster
 
 class SOStream:
 
-    def __init__(self, alpha = 0.1, min_pts = 10):
+    def __init__(self, alpha = 0.1, min_pts = 10, merge_threshold = 27000):
         self.alpha = alpha
         self.min_pts = min_pts
-        self.M = list()
+        self.M = [[]]
+        self.merge_threshold = merge_threshold
 
     def process(self, vt):
         winner_micro_cluster = min_dist(vt, self.M[-1])
-        if len(self.M[-1]) >= self.min_pts:
-            winner_neighborhood = find_neighbors(winner_micro_cluster, self.min_pts, self.M[-1])
-            if dist(vt, winner_micro_cluster['C'] < winner_micro_cluster['r']):
+        new_M = self.M[-1].copy()
+        if len(new_M) >= self.min_pts:
+            winner_neighborhood = find_neighbors(winner_micro_cluster, self.min_pts, new_M)
+            if dist(vt, winner_micro_cluster['C']) < winner_micro_cluster['r']:
                 updateCluster(winner_micro_cluster, vt, self.alpha, winner_neighborhood)
             else:
-                newCluster(self.M, vt)
+                new_M.append(newCluster(vt))
             overlap = find_overlap(winner_micro_cluster, winner_neighborhood)
             if len(overlap) > 0:
-                merge_clusters(winner_micro_cluster, overlap)
+                merged_cluster, deleted_clusters = merge_clusters(winner_micro_cluster, overlap, self.merge_threshold)
+                for deleted_cluster in deleted_clusters:
+                    new_M.remove(deleted_cluster)
+                new_M.append(merged_cluster)
         else:
-            newCluster(self.M, vt)
-
+            new_M.append(newCluster(vt))
+        self.M.append(new_M)
     pass
